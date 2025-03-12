f4522734b9a4636295b6f99356161fdde9b5a5eb
fr0zen <176347124+fr0zenStore@users.noreply.github.com>
2025-03-12 23:54:47 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="bg-dark text-white text-center p-3">
+    <p>&copy; <?php echo date('Y'); ?> Bootspeed - Tema ultraleggero e veloce</p>
+    <?php wp_footer(); ?>
+</footer>
+</body>
+</html>

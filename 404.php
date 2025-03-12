de3cbab4372e35ea37037de2a4b046b007743a89
fr0zen <176347124+fr0zenStore@users.noreply.github.com>
2025-03-12 23:57:53 +0100

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<div class="container py-5 text-center">
+    <h1>Errore 404</h1>
+    <p>Pagina non trovata.</p>
+    <a href="<?php echo home_url(); ?>" class="btn btn-primary">Torna alla home</a>
+</div>
+<?php get_footer(); ?>
